5c101c4a1ec9182e23bd3eafa8d9e6088e085463
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-05-15 13:01:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33ab"
+
+//console.log(typeof (score) );
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber) );
+
+// console.log(valueInNumber);  // o/p => NaN as it is not pure number and in case of undefined also
+                             //  o/p => 0 in case of null
+
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1
+// rishabh =>NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0=> false
+// "" => false
+// "rishbah" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
