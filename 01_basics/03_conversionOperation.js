b0fa2f3a9cb6d35583450f4442006d78cb7f4d71
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-05-20 17:17:23 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,6 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(stringNumber)      //  => 33
+console.log(typeof stringNumber)   //=> String(Number Converted to String)
 
