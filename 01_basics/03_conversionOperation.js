c96cc0bee509efde63b80ff444cdc9040de9699f
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-05-21 11:59:05 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)      //  => 33
-console.log(typeof stringNumber)   //=> String(Number Converted to String)
+// console.log(stringNumber)      //  => 33
+// console.log(typeof stringNumber)   //=> String(Number Converted to String)
+
+
+
+
+// *****************************************************Operations******************************************************************************************************************************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);  //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  // 2^3
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2= " Rishabh"
+
+let str3 = str1+str2;
+// console.log(str3);     // => Hello Rishabh
+
+// console.log("1"+2)   // 12
+// console.log(1+"2")   // 12
+// console.log("1"+2+2) // 122
+// console.log(1+2+"2") // 32
+
+// console.log((3+4)*5%3);
+
+
+// console.log(+true);  // 1
+// console.log(+"");    // 0
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2;
+
+let gameCounter = 100
+gameCounter++;   // badme increment ho jati hai
+++gameCounter;   // pehle increment ho jati hai
+console.log(gameCounter);
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);  // true
+// console.log("02">1);  // true
+
+console.log(null>0);  // false
+console.log(null==0); // false
+console.log(null>=0); // true
+                  //////////////////////////////////// == and >= works differenty in JS
+console.log(undefined==0);  // false
+console.log(undefined>0);   // false
+console.log(undefined<0);   // false
+
+// === Strict Check checks data type also
+
+console.log("2"===2)  // false
