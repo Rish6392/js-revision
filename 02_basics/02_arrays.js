88a0355012b17e3721c2e7355aad61e8f9c14837
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-05-24 12:51:23 +0000

    Array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,37 @@
+const marvel_heroes = ["thor","Ironman","spiderman"];
+const dc = ["superman","flash","batman"];
+
+// marvel_heroes.push(dc);
+
+// console.log(marvel_heroes); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heroes[3][1]);    // flash
+
+// const allHero = marvel_heroes.concat(dc);  //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// console.log(allHero);
+
+const all_new_hero = [...marvel_heroes,...dc]; // Spread operator 
+// [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// console.log(all_new_hero);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+const real_another_array = another_array.flat(Infinity);
+// [1, 2, 3, 4, 5,6, 7, 6, 7, 4,5]
+// console.log(real_another_array);
+
+
+
+
+
+////////////////////////**********Interview */
+// console.log(Array.isArray("Hitesh"))     // false 
+// console.log(Array.from("Hitesh"))       // [ 'H', 'i', 't', 'e', 's', 'h' ]
+// console.log(Array.from({name:"hitesh"}))    // []
+
+let score1= 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));  //[ 100, 200, 300 ]
+
+
