3c9c0baff224db570054a6613556ffc3a7fbba9c
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-05-25 21:23:32 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,6 +2,19 @@
 
 // alert(3+3)  // we are using node js not browser
 
+//////////////////////////////////////////////////////*********INTERVIEW****************/////////////////////////////////////////////////////////////////////////////
+/*
+number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+*/
+///////////////////////////////////////////////////////*****INTERVIEW****************////////////////////////////////////////////////////////////////////////////
+
+
 let name = "rishabh"
 let age=20
 let isLoggedIn = false
