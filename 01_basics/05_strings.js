507962ef90fec52f27ccdef48957d902efd7133f
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-05-26 17:15:04 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,7 +3,7 @@ const repoCount = 50
 
 // console.log(name+repoCount+" Value");  // Rishabh50 Value
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  *******USE THIS *************
 
 const gameName = new String('hiteshhc');
 
@@ -11,7 +11,7 @@ const gameName = new String('hiteshhc');
 // console.log(gameName.__proto__);  //{}
 
 // console.log(gameName.length);  // 8
-// console.log(gameName.toUpperCase()); //  HITESHHC
+// console.log(gameName.toUpperCase()); //  HITESHHC 
 
 // console.log(gameName.charAt(2));  // t
 
