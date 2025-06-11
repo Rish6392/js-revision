655d8df4230c1034545f0b03d2fed49049747894
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-06-11 11:43:09 +0000

    while loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -41,7 +41,7 @@ for (let index = 1; index <=20; index++) {
 for (let index = 1; index <=20; index++) {
     if(index==5){
         console.log("Detected 5");
-        continue;   / ek barke liye skip
+        continue;   // ek barke liye skip
     }
     console.log(`Value of i is ${index}`);
 
